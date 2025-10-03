780707845b709428b1f5a113b481e41a28128af1
George Ogden <george-ogden@outlook.com>
2025-10-03 16:50:49 +0200

    Sort unordered collections during assert

--- src/plugin.py
+++ src/plugin.py
@@ -1,16 +1,18 @@
+import functools
 from typing import Any
 
 from _pytest.assertion.util import _compare_eq_any
-from debug._format import Formatter, FormatterConfig
+from debug import pformat
 from pytest import Config
 
 
 def pytest_assertrepr_compare(config: Config, op: str, left: Any, right: Any) -> list[str] | None:
     if op == "==":
-        formatter_config = FormatterConfig(indent_width=2, terminal_width=0, color=False)
-        formatter = Formatter(formatter_config)
-        left_repr = formatter.format(left, initial_width=0)
-        right_repr = formatter.format(right, initial_width=0)
+        format = functools.partial(
+            pformat, width=0, style=None, indent=2, sort_unordered_collections=True
+        )
+        left_repr = format(left)
+        right_repr = format(right)
 
         if left_repr != right_repr:
             highlighter = config.get_terminal_writer()._highlight
